b0ebdda9e533dd32047c2c9255b22f741675d44f
SeungYn <kwls0407@gmail.com>
2024-02-07 23:08:27 +0900

    feat: AboutMe 컴포넌트 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Introduce from '@/components/Introduce/Introduce';
+import AboutMe from '@/components/aboutme/AboutMe/AboutMe';
 import Header from '@/components/header/Header';
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <Header />
       <div className='max-w-screen-lg mx-auto px-20'>
         <Introduce />
+        <AboutMe />
       </div>
     </main>
   );
