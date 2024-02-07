41a894378fbc01cdac6f1fcdde74d2fcdac9dbf2
SeungYn <kwls0407@gmail.com>
2024-02-07 15:31:35 +0900

    feat: tailwindcss color 등록

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'dark-navy': '#020c1b',
+        navy: '#0a192f',
+        'light-navy': '#112240',
+        'lightest-navy': '#233554',
+        'navy-shadow': 'rgba(2, 12, 27, 0.7)',
+        'dark-slate': '#495670',
+        slate: '#8892b0',
+        'light-slate': '#a8b2d1',
+        'lightest-slate': '#ccd6f6',
+        white: '#e6f1ff',
+        green: '#64ffda',
+        'green-tint': 'rgba(100, 255, 218, 0.1)',
+        pink: '#f57dff',
+        blue: '#57cbff',
       },
     },
   },
