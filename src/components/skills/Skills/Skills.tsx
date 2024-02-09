900fb0f55c448388d5ed89a0a913e0c487bd7359
SeungYn <kwls0407@gmail.com>
2024-02-09 18:40:14 +0900

    refactor: 테일윈드 프리티어 적용

--- src/components/skills/Skills/Skills.tsx
+++ src/components/skills/Skills/Skills.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import SectionContainer from '@/components/common/section/SectionContainer/SectionContainer';
-import styles from './Skills.module.css';
-import { useState } from 'react';
-import SkillContent from '../SkillContent/SkillContent';
+import SectionContainer from "@/components/common/section/SectionContainer/SectionContainer";
+import styles from "./Skills.module.css";
+import { useState } from "react";
+import SkillContent from "../SkillContent/SkillContent";
 
 export const SkillCategoryList = [
-  { index: 0, title: 'Language', class: styles.skillTabActiveter0 },
-  { index: 1, title: 'FrontEnd', class: styles.skillTabActiveter1 },
-  { index: 2, title: 'BackEnd', class: styles.skillTabActiveter2 },
-  { index: 3, title: 'Database', class: styles.skillTabActiveter3 },
-  { index: 4, title: 'ETC', class: styles.skillTabActiveter4 },
+  { index: 0, title: "Language", class: styles.skillTabActiveter0 },
+  { index: 1, title: "FrontEnd", class: styles.skillTabActiveter1 },
+  { index: 2, title: "BackEnd", class: styles.skillTabActiveter2 },
+  { index: 3, title: "Database", class: styles.skillTabActiveter3 },
+  { index: 4, title: "ETC", class: styles.skillTabActiveter4 },
 ];
 
 export default function Skills() {
@@ -18,17 +18,17 @@ export default function Skills() {
 
   return (
     <SectionContainer
-      title='Skills'
-      count='02.'
-      className='mx-0 md:mx-6 min-h-[500px]'
+      title="Skills"
+      count="02."
+      className="mx-0 min-h-[500px] md:mx-6"
     >
-      <div className='flex flex-col  md:flex-row '>
+      <div className="flex flex-col md:flex-row ">
         <div className={styles.tabList}>
           {SkillCategoryList.map((obj) => (
             <button
               key={obj.title}
               className={`${styles.tabBtn} ${
-                obj.index === currentTitleIndex ? 'text-green' : ''
+                obj.index === currentTitleIndex ? "text-green" : ""
               }`}
               onClick={() => setCurrentTitleIndex(obj.index)}
             >
@@ -41,7 +41,7 @@ export default function Skills() {
           ></div>
         </div>
 
-        <div className='w-full ml-8 relative mt-6 md:mt-0'>
+        <div className="relative ml-8  mt-6 w-full md:mt-0">
           <SkillContent categoryIndex={currentTitleIndex} />
         </div>
       </div>
