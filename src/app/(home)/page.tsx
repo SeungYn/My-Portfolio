af6b7ed8ebd6de4164d431f528faa1a4c9995753
SeungYn <kwls0407@gmail.com>
2024-02-09 17:23:32 +0900

    design: layout css 수정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,18 @@
 import Introduce from '@/components/Introduce/Introduce';
 import AboutMe from '@/components/aboutme/AboutMe/AboutMe';
 import Header from '@/components/header/Header';
+import Skills from '@/components/skills/Skills/Skills';
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <div className='max-w-screen-lg mx-auto px-20'>
-        <Introduce />
-        <AboutMe />
+      <div className='max-w-screen-xl mx-auto px-20'>
+        <div className=' mx-auto max-w-[1000px]'>
+          <Introduce />
+          <AboutMe />
+          <Skills />
+        </div>
       </div>
     </main>
   );
