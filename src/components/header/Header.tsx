7a1511de951008b4c7ad8ede93b40eac8fb2e0cd
SeungYn <kwls0407@gmail.com>
2024-02-14 16:38:23 +0900

    feat: 헤더 애니메이션 적용

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,10 +3,33 @@ import { FadeDown } from '@/constant/framer-motion';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+	const [isHide, setIsHide] = useState(false);
+
+	useEffect(() => {
+		let lastScrollY = window.scrollY;
+		const onScroll = () => {
+			const differenceY = lastScrollY - window.scrollY;
+
+			if (!!differenceY === isHide) return;
+			if (differenceY >= 0) setIsHide(false);
+			else setIsHide(true);
+			lastScrollY = window.scrollY;
+		};
+
+		window.addEventListener('scroll', onScroll);
+
+		return () => {
+			window.removeEventListener('scroll', onScroll);
+		};
+	}, []);
+
 	return (
-		<header className="fixed w-full">
+		<header
+			className={`bg-['rgba(10, 25, 47, 0.85)'] fixed z-30 w-full backdrop-blur-lg transition-all ${isHide ? '-translate-y-full' : 'translate-y-0'}`}
+		>
 			<nav className="flex items-center justify-between px-10 py-8">
 				<Link href="/">
 					<Image
@@ -18,7 +41,7 @@ export default function Header() {
 				</Link>
 
 				<motion.ol
-					className="flex gap-6 text-sm [&>a>li>span]:text-green [&>a>li]:flex [&>a>li]:gap-1"
+					className="flex gap-6 text-base [&>a>li>span]:text-green [&>a>li]:flex [&>a>li]:gap-1"
 					initial={'init'}
 					animate={'on'}
 					transition={{ staggerChildren: 0.1 }}
@@ -47,9 +70,9 @@ export default function Header() {
 							<p>Contact</p>
 						</motion.li>
 					</Link>
-					<li>
+					{/* <li>
 						<button>Resume</button>
-					</li>
+					</li> */}
 				</motion.ol>
 			</nav>
 		</header>
