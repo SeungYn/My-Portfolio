ed4a787b0c7959aef2365e13ad9a58a8f1c2378f
SeungYn <kwls0407@gmail.com>
2024-02-15 14:11:31 +0900

    feat: id constant 작성

---
+++ src/constant/sectionId.ts
@@ -0,0 +1,8 @@
+export const SectionID = {
+	aboutMe: 'about-me',
+	skills: 'skills',
+	mainProjects: 'main-projects',
+	subProjects: 'sub-projects',
+	contact: 'contact',
+	mainContent: 'main-content',
+};
