2f2162d273951f37de92ed4b3d2d429cd795af89
SeungYn <kwls0407@gmail.com>
2024-02-15 14:12:11 +0900

    refactor: console.log 제거

--- src/hooks/useOnClickOutside.tsx
+++ src/hooks/useOnClickOutside.tsx
@@ -6,12 +6,6 @@ export default function useOnClickOutside(
 ) {
 	useEffect(() => {
 		const cb = (e: MouseEvent) => {
-			console.log(
-				(e.target as HTMLElement).parentNode,
-				(e.target as HTMLElement).parentNode?.parentNode
-			);
-			console.log(e.target);
-			console.log(ref, ref!.current!.contains(e.target as Node));
 			if (!ref?.current || ref.current.contains(e.target as Node)) return;
 
 			callback();
