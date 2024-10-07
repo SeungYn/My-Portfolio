8276c5af9aa11eacb5d1b346d480c53f4d501ece
SeungYn <kwls0407@gmail.com>
2024-10-07 20:22:06 +0900

    fix: ga 수정

--- src/components/common/analytics/GoogleAnalytics.tsx
+++ src/components/common/analytics/GoogleAnalytics.tsx
@@ -3,10 +3,9 @@ import Script from 'next/script';
 export function GoogleAnalytics() {
 	return (
 		<>
-			{/* <!-- Google Tag Manager (noscript) --> */}
 			<noscript>
 				<iframe
-					src={`https://www.googletagmanager.com/ns.html?id=${process.env.G_ID}`}
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.G_ID}`}
 					height="0"
 					width="0"
 					className="invisible hidden"
@@ -14,12 +13,12 @@ export function GoogleAnalytics() {
 			</noscript>
 			{/* <!-- End Google Tag Manager (noscript) --> */}
 			{/* <!-- Google Tag Manager --> */}
-			<Script id="google-tag-management">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer',"${process.env.G_ID}");
+			<Script id="" strategy="lazyOnload">{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', '${process.env.G_ID}');
 `}</Script>
 		</>
 	);
